c5c01839bc68a3f36b4b801341494696e02605ce
abdallahshaban <abdallah.w.shaban@gmail.com>
2025-09-19 14:47:01 -0700

    initial_commit

---
+++ bin/youtube_to_blog.dart
@@ -0,0 +1,5 @@
+import 'package:youtube_to_blog/youtube_to_blog.dart' as youtube_to_blog;
+
+void main(List<String> arguments) {
+  print('Hello world: ${youtube_to_blog.calculate()}!');
+}
---
+++ test/youtube_to_blog_test.dart
@@ -0,0 +1,8 @@
+import 'package:youtube_to_blog/youtube_to_blog.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
